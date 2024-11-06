0a718deab378e2c9f77df768e2c21af817ad8164
Ishuwara Avarjana <ishuwarawedage@gmail.com>
2024-11-06 22:33:49 +0530

    react project create

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/shared/Footer";
+import Navbar from "../components/shared/Navbar";
+
+const Home = () => {
+    return ( 
+        <div>
+            <Navbar />
+            sup
+            <Footer />
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // "./src/App.jsx",
+    // "./src/pages/**.jsx",
+    // "./src/components/**/**.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
